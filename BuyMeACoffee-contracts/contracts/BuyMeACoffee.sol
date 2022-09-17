c26bd70681de052ee63610cd2c7015c0def3570d
Meshack Mbuvi <meshmbuvi@gmail.com>
2022-09-17 19:16:04 +0300

    Add contracts and tests for Buy Me A Coffee
    This change adds contracts(scripts) and tests for buying a contract owner
    coffee. To buy a coffee, the user will send some ETH in form of a tip and
    a message. There is a also a feature to retrieve all messages that have
    been sent from users and another to withdraw all the tips that have been
    received from users.

---
+++ BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
@@ -0,0 +1,58 @@
+/// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.0;
+
+contract BuyMeACoffee {
+    /// Event to emti when a Memo is created.
+    event NewMemo (
+        address indexed from, 
+        uint256 timestamp, 
+        string name,
+        string message
+        );
+
+    /// Memo struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    /// Address of contract deployer. Marked payable so that we can withdraw to
+    /// this later.
+    address payable owner;
+    Memo [] memos;
+
+    constructor(){
+        /// store the address of the deployer as a payable address.
+        owner = payable(msg.sender);
+    }
+
+    /// @dev fetches all stored memos
+    function getMemos() public view returns (Memo [] memory) {
+        return memos;
+    }
+
+    /// @dev buy a coffee for owner (sends an ETH tip and leaves a memo)
+    /// @param _name name of the coffee purchaser
+    /// @param _message a nice message from the purchaser
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        /// Must accept more that 0 ETH for a coffee
+        require(msg.value > 0, "Can't buy coffee for free!");
+
+        /// Add the memo to storage
+        memos.push(
+            Memo(msg.sender,
+            block.timestamp,
+            _name,
+            _message));
+
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+    }
+
+    /// @dev send the entire balance stored in this contract to the owner.
+    function withdrawTips()public {
+        require(owner.send(address(this).balance));
+    }
+}
