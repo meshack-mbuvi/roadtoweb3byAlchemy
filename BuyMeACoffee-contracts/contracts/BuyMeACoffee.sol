93ea2981a66049c54aba04704eaea22b725d70db
Meshack Mbuvi <meshmbuvi@gmail.com>
2022-09-18 18:45:39 +0300

    Add new method to update withdrawal address.

--- BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
+++ BuyMeACoffee-contracts/contracts/BuyMeACoffee.sol
@@ -38,7 +38,7 @@ contract BuyMeACoffee {
     /// @param _name name of the coffee purchaser
     /// @param _message a nice message from the purchaser
     function buyCoffee(string memory _name, string memory _message) public payable {
-        /// Must accept more that 0 ETH for a coffee
+        /// Must accept more than 0 ETH for a coffee
         require(msg.value > 0, "Can't buy coffee for free!");
 
         /// Add the memo to storage
@@ -54,5 +54,11 @@ contract BuyMeACoffee {
     /// @dev send the entire balance stored in this contract to the owner.
     function withdrawTips()public {
         require(owner.send(address(this).balance));
+    }
+
+    /// @dev Update withdrawal address.
+    function changeOwnerAddress (address _newOwnerAddress) public {
+        require(owner == msg.sender, "Only contract owner can change owner address");
+        owner = payable(_newOwnerAddress);
     }
 }
